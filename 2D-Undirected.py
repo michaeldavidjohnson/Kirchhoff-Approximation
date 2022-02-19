13e106dba604a0d0592c26e072f6ef8743434bdf
Michael-David Johnson <70021788+michaeldavidjohnson@users.noreply.github.com>
2022-02-19 20:33:56 +0000

    Update 2D-Undirected.py

--- 2D-Undirected.py
+++ 2D-Undirected.py
@@ -59,10 +59,21 @@ class Undirected2DVectorised:
             self.surfaceVals = self.surfaceVals + np.zeros((self.number,self.samples))
             self.derivativeVals = self.derivativeVals + np.zeros((self.number,self.samples))
     
-    def surfaceChecker(self, relaxed = True):
-        #self.doubleDerivativeVals = sp.misc.derivative(self.surfaceFunction,self.x,n=2)
-        numerator = 1 + (self.derivativeVals)**2
-        denominator = self.doubleDerivativeVals
+    def surfaceChecker(self, relaxed = True, hyper_accurate = False):
+        if not hyper_accurate:
+            #self.doubleDerivativeVals = sp.misc.derivative(self.surfaceFunction,self.x,n=2)
+            numerator = 1 + (self.derivativeVals)**2
+            denominator = self.doubleDerivativeVals
+        
+        else:
+            #For highly oscillatory functions
+            x = np.linspace(self.min, self.max, 10*self.samples)
+            fun_val = self.surfaceFunction(x)
+            derivativeVals = np.gradient(fun_val, x[1] - x[0],edge_order=2, axis=None)
+            doublederivativeVals = np.gradient(derivativeVals, x[1] - x[0],edge_order=2, axis=None)
+            numerator = 1 + derivativeVals**2
+            denominator = doublederivativeVals
+
         self.curvature = (numerator**1.5)/np.abs((denominator))
         self.condition = 1/((self.k*self.curvature)**0.333333333)
         print(self.condition.max())
